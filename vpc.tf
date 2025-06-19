ec5e1b878fce6c459c38889c85d8b9dd9886be26
Rishikesh Tembhurne <rishikeshtembhurne@Rishikeshs-MacBook-Air.local>
2025-06-19 22:49:06 +0530

    adding vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,36 @@
+locals {
+  cluster_name = "rishi-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
+module "vpc" {
+  source               = "terraform-aws-modules/vpc/aws"
+  version              = "6.0.0"
+  name                 = var.vpc_name
+  cidr                 = var.vpc_cidr
+  azs                  = var.azs
+  private_subnets      = var.private_subnets
+  public_subnets       = var.public_subnets
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support   = var.enable_dns_support
+  enable_nat_gateway   = var.enable_nat_gateway
+  single_nat_gateway   = var.single_nat_gateway
+  tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb"                      = 1
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb"             = 1
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
+}
+
